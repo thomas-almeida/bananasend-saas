5f7b4fd82a954c947c67d02a09f9ceee3ce02825
thomas almeida <contato.thomasalmeidard@gmail.com>
2025-09-20 18:01:34 -0300

    Onboarding Field

--- backend/src/db/models/User.js
+++ backend/src/db/models/User.js
@@ -3,11 +3,19 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
   username: String,
   email: { type: String, unique: true },
-  avatar: String,
+  // avatar removido para refletir estrutura de produção
   subscriptionId: { type: String, default: "FREE" },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
-  mails: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Mails' }]
+  mails: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Mails' }],
+  onboarding: {
+    age: Number,
+    linkedinUrl: String,
+    occupation: String,
+    workspace: String,
+    mail: String,
+    password: String,
+  }
 });
 
 const User = mongoose.model("User", UserSchema);
